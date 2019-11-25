f9d81f24d38ddd94e71fba74b301060e0e982187
sean-niemann <snemosdn@aol.com>
2019-11-25 09:36:14 -0700

    Updated version to 2.3.2

--- src/org/nemotech/rsc/Constants.java
+++ src/org/nemotech/rsc/Constants.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class Constants {
     
-    public static final String VERSION = "2.3.1";
+    public static final String VERSION = "2.3.2";
     
     public static int EXPERIENCE_MULTIPLIER = 8;
 
